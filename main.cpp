0a9abebae6c5a173bd2c19862647ec32f3489016
Daniel Hambleton <daniel.hambleton@meshconsultants.ca>
2017-04-04 15:58:46 -0400

    getting all kinds of unresolved external links..

--- main.cpp
+++ main.cpp
@@ -25,18 +25,82 @@
 #include <vector>
 #include <string>
 
+//#include <examples_linking_pragmas.h>
+
+
 #include <rapidjson.h>
-#include <opennurbs.h>
+#include "opennurbs.h"
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
 	
-	cout << "ready to go" << endl;
-	
-	
-	cin.get();
+	// Call once in your application to initialze opennurbs library
+	ON::Begin();
+
+	//default dump is to stdout
+	ON_TextLog dump_to_stdout;
+	dump_to_stdout.SetIndentSize(2);
+	ON_TextLog* dump = &dump_to_stdout;
+	FILE* dump_fp = 0;
+
+	// open file containing opennurbs archive
+	ON_wString ws_arg = "../../test/test_data.3dm";
+	const wchar_t* wchar_arg = ws_arg;
+
+	FILE* archive_fp = ON::OpenFile(wchar_arg, L"rb");
+	if (!archive_fp)
+	{
+		cout << "Unable to read file" << endl;
+		return 0;
+	}
+
+	//on model
+	ONX_Model model;
+
+	// create achive object from file pointer
+	ON_BinaryFile archive(ON::read3dm, archive_fp);
+
+	// read the contents of the file into "model"
+	bool rc = model.Read(archive, dump);
+
+	// close the file
+	ON::CloseFile(archive_fp);
+
+	// close the file
+	ON::CloseFile(archive_fp);
+
+	// print diagnostic
+	if (rc)
+		dump->Print("Successfully read.\n");
+	else
+		dump->Print("Errors during reading.\n");
+
+	// see if everything is in good shape
+	if (model.IsValid(dump))
+	{
+		dump->Print("Model is valid.\n");
+	}
+	else
+	{
+		dump->Print("Model is not valid.\n");
+	}
+
+	// create a text dump of the model
+	bool verbose = true;
+	if (verbose)
+	{
+		dump->PushIndent();
+		model.Dump(*dump);
+		dump->PopIndent();
+	}
+
+	// destroy this model
+	model.Destroy();
+
+	dump->PopIndent();
+
 
 	return 0;
 }
